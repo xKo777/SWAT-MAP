370f93bf5e4cc43c467d787be522f5b9b4b8d055
xKo777 <hellomello12853@gmail.com>
2026-03-22 21:40:26 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,1 @@
-const map = L.map('map', {
-  crs: L.CRS.Simple,
-  minZoom: -2
-});
-
-const bounds = [[0,0], [1000,1000]];
-L.imageOverlay('map.jpg', bounds).addTo(map);
-map.fitBounds(bounds);
+console.log("Firebase Test:", firebase);
